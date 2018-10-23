9250131dda88285ee923169fd309d8fcc4b2dab3
Imad <ibelkebir@stuy.edu>
2018-10-22 23:21:08 -0400

    not faster

--- sieve.c
+++ sieve.c
@@ -66,78 +66,7 @@ int sieve(int targetPrime){
   return potentialPrime(index);
 }
 
-int fastsieve(int targetPrime){
-  if(!(targetPrime ^ 1)){
-    return 2;
-  }
-  if(!(targetPrime ^ 2)){
-    return 3;
-  }
-  if(!(targetPrime ^ 3)){
-    return 5;
-  }
-  if(!(targetPrime ^ 4)){
-    return 7;
-  }
-  if(!(targetPrime ^ 5)){
-    return 11;
-  }
-  int len;
-  if(targetPrime < 5000){
-    len = .4 * targetPrime * (int)log(targetPrime);
-  }else{
-    len = .398 * targetPrime * (int)log(targetPrime);
-  }
-  int* cur = calloc(len / 32, sizeof(int));
-  int index = 3;
-  int i = 3;
-  int num;
-  int square;
-  int next;
-  int range;
-  int limit = (int)sqrt(len) / 1.73;
-  while(index < limit){
-    if(!(cur[index / 32] & (1 << (index & 31)))){
-      num = potentialPrime(index);
-      if(num % 5 && num % 7 && num % 11){
-        square = ptoi(num * num);
-        next = num << 1;
-        if((square - index) & 1){
-          range = num + (num+1) / 3;
-        }else{
-          range = num - (num+1) / 3;
-        }
-        while(square < len){
-          cur[square / 32] |= (1 << (square & 31));
-          range = next - range;
-          square += range;
-        }
-        i++;
-      }
-    }
-    index++;
-  }
-  while(i ^ targetPrime - 1){
-    if(!(cur[index / 32] & (1 << (index & 31)))){
-      num = potentialPrime(index);
-      if(num % 5 && num % 7 && num % 11){
-        i++;
-      }
-    }
-    index++;
-  }
-  while(cur[index / 32] & (1 << (index & 31))){
-    index++;
-  }
-  num = potentialPrime(index);
-  while(cur[index / 32] & (1 << (index & 31)) || !(num % 5) || !(num % 7) || !(num % 11)){
-    index++;
-    num = potentialPrime(index);
-  }
-  return potentialPrime(index);
-}
-
 int main(){
-  printf("%d\n", fastsieve(1000000));
+  printf("%d\n", sieve(1000000));
   return 0;
 }
